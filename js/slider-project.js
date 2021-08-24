c65cd769ee721e2d90118666c946e9f600db41a2
JorgeMantinan <jorgemantinanmontero@gmail.com>
2021-08-24 13:56:46 +0200

    Design of slider-project

---
+++ js/slider-project.js
@@ -0,0 +1,13 @@
+var projectImgs = [
+    {1: "",2: "",3: "",4:""},
+    {1: "",2: "",3: ""},
+    {1: ""},
+    {1: "",2: ""}
+];
+
+var project = [
+    {img: projectImgs[1], description: "Descripción del proyecto 1"},
+    {img: projectImgs[2], description: "Descripción del proyecto 2"},
+    {img: projectImgs[3], description: "Descripción del proyecto 3"},
+    {img: projectImgs[4], description: "Descripción del proyecto 4"}
+];
